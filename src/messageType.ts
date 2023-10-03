596f7323329d51fe42a185fd77db5102e4b1ad26
Noah <nomawo@yahoo.com>
2023-10-03 12:32:31 -0400

    ability to override auto generated exchange name

--- src/messageType.ts
+++ src/messageType.ts
@@ -1,27 +1,39 @@
 export class MessageType {
-    private static defaultNamespace: string = 'Messages';
+  private static defaultNamespace: string = "Messages";
+  name: string;
+  ns: string;
+  exchange: string | undefined;
+
+  constructor({
+    name,
+    ns,
+    exchange,
+  }: {
     name: string;
-    ns: string;
+    ns?: string;
+    exchange?: string;
+  }) {
+    this.name = name;
+    this.ns = ns ?? MessageType.defaultNamespace;
+    this.exchange = exchange;
+  }
 
-    constructor(name: string, ns?: string) {
-        this.name = name;
-        this.ns = ns ?? MessageType.defaultNamespace;
-    }
+  static setDefaultNamespace(ns: string) {
+    this.defaultNamespace = ns;
+  }
 
-    static setDefaultNamespace(ns: string) {
-        this.defaultNamespace = ns;
-    }
+  toString(): string {
+    return `urn:message:${this.ns}:${this.name}`;
+  }
 
-    toString(): string {
-        return `urn:message:${this.ns}:${this.name}`;
-    }
+  toMessageType(): Array<string> {
+    return [this.toString()];
+  }
 
-    toMessageType(): Array<string> {
-        return [this.toString()];
+  toExchange(): string {
+    if (this.exchange) {
+      return `${this.exchange}`;
     }
-
-    toExchange(): string {
-        return `${this.ns}:${this.name}`
-    }
+    return `${this.ns}:${this.name}`;
+  }
 }
-
